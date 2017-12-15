7365c946ff47bfc3a581bd271059827ba33c4800
WhiteWalker98 <sanidhyarsharma@gmail.com>
2017-12-15 19:31:03 +0530

    Updated Readme

--- Android Getting Started with Firebase - Login and Registration Authentication_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"<b>{num}<\/b> Comments","one":"<b>1<\/> Comment"}},"counts":[{"id":"https:\/\/www.androidhive.info\/2016\/06\/android-getting-started-firebase-simple-login-registration-auth\/","comments":376}]});
-}
